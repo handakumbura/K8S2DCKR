583269cd14d313818b7181952e721af6806e0173
handakumbura <dumiduh@gmail.com>
2017-10-04 17:16:35 +0530

    adds MIT license headers.

--- init.py
+++ init.py
@@ -1,3 +1,13 @@
+'''
+Copyright 2017 Dumidu Handakumbura
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
 import prerequisite.systemprerequisite as syscheck
 import db.dbwrapper
 import os
@@ -24,9 +34,7 @@ def setup_logging():
         with open('config/logging_config.yaml', 'rt') as f:
             config = yaml.load(f)
         logging.config.dictConfig(config)
-    else:
-        logging.basicConfig(level=logging.DEBUG)
-        logging.error('k8s2dckr logging setup failed.')
+
 
 
 if __name__ == "__main__":
